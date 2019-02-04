77bb6f40db030e3e85f09f04ac9625b466a0e5da
Dawid Dyczek <wizard91@o2.pl>
2019-02-04 12:06:06 +0000

    indentation fix

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component  {
-    constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = { term: ''};
-    }
- render() {
-     return (
-         <div className="search-bar">
-            <input
-            value={this.state.term}
-            onChange={event => this.onInputChange(event.target.value)} />
-         </div>
+  }
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)} />
+       </div>
     );
   }
 onInputChange(term) {
