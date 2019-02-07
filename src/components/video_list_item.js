61c6f942ad919f757b4a0583a2aaa3b04fdefa9e
Dawid Dyczek <wizard91@o2.pl>
2019-02-07 12:09:59 +0000

    small fixes

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -6,10 +6,10 @@ const VideoListItem = ({video, onVideoSelect }) => {
     <li onClick={() => onVideoSelect(video)} className="list-group-item">
         <div className="video-list-media">
           <div className="media-left">
-                    <img className="media-object" src={imageUrl} />
-                </div>
-                <div className="media-body">
-                    <div className="media-heading">
+            <img className="media-object" src={imageUrl} />
+          </div>
+          <div className="media-body">
+            <div className="media-heading">
                     {video.snippet.title}
                     </div>
                 </div>
