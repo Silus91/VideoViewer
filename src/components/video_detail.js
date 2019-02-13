c0fc3296b7d95120d884cbea576e3a19b969c698
Dawid Dyczek <wizard91@o2.pl>
2019-02-13 22:01:30 +0000

    small update

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -15,8 +15,8 @@ const VideoDetail = ({video}) => {
         </iframe>
       </div>
       <div className="details">
-          <div>{video.snippet.title}</div>
-          <div>{video.snippet.description}</div>
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
       </div>
     </div>
   );
